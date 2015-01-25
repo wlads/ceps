caa753fc5168521296a24d2b3b5bdc8e0a0bf2f7
Talles L <talleslasmar@gmail.com>
2015-01-25 14:29:36 -0200

    test action

--- bin/ceps.js
+++ bin/ceps.js
@@ -60,9 +60,25 @@ mongo.connect(function (err, db) {
     })
   }
 
+  function testCep (req, res) {
+    correiosCrawler('30130010', function (err, endereco) {
+
+      // if there's an error crawling Correios' website
+      if (internalError(err, res)) ;
+
+      // if not found
+      else if (notFound(endereco, res)) ;
+
+      // if crawled
+      else res.sendStatus(200)
+
+    })
+  }
+
   var app = express()
   app.use(morgan('short'))
   app.get('/', redirectToGitHub)
+  app.get('/test', testCep)
   app.get('/:cep', serveCep)
   app.disable('x-powered-by')
   app.listen(process.env.PORT || 80)
