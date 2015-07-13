a880be8588aeeac9f3916fd186e75269e25bbed2
Talles L <talleslasmar@gmail.com>
2015-07-13 00:45:41 -0300

    using envvar package

--- bin/ceps.js
+++ bin/ceps.js
@@ -1,10 +1,8 @@
 #! /usr/bin/env node
 
-var configuration = require('../lib/configuration')
-  , persistence   = require('../lib/persistence')
-  , web           = require('../lib/web')
+var persistence = require('../lib/persistence')
+  , web         = require('../lib/web')
 
-configuration.check()
 persistence.connect(function (err, mongo) {
   if (err) throw err
   web(mongo)
